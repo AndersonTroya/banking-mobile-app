6a51e0c042d526bdd1cca329c3b54d3683c3e4de
Anderson <andavidtr@gmail.com>
2025-11-26 17:31:34 -0500

    clases

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Splash from "./src/screens/splash";
+import Register from "./src/screens/register";
+import Login from "./src/screens/login";
 
 export default function App() {
   const [showSplash, setShowSplash] = useState(true);
   if (showSplash){
     return <Splash onFinish={() => setShowSplash(false)}/>
   }
+
+  return <Login/>
 }
--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,7 +1,7 @@
 import 'react-native-url-polyfill/auto';
-import {createClient}
+import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = '';
-const SUPABASE_kEY = '';
+const SUPABASE_URL = 'https://camdqsnlbznxfttsnhph.supabase.co';
+const SUPABASE_kEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhbWRxc25sYnpueGZ0dHNuaHBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyOTk4NDYsImV4cCI6MjA3Mzg3NTg0Nn0.OMTZz3AjSoyNtwJvKkV9CuH5adEh0yYvtZ0fP-Tifvs';
 
-export const supabase = 
+export const supabase = createClient(SUPABASE_URL, SUPABASE_kEY);
